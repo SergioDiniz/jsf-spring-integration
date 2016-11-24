b29182817fa4ef6383bd4ef706abce43c8e2cacb
Sergio Diniz <sergiodinizsh@gmail.com>
2016-11-24 08:06:41 -0300

    error

--- src/br/com/beans/User.java
+++ src/br/com/beans/User.java
@@ -1,15 +1,19 @@
 package br.com.beans;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="users")
 public class User {
 
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	@Column(name="name")
 	private String name;
 
 	public User() {
@@ -31,6 +35,10 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	
+
+	
 	
 	
 	
--- src/br/com/dao/UserDaoImp.java
+++ src/br/com/dao/UserDaoImp.java
@@ -3,9 +3,15 @@ package br.com.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +20,28 @@ import br.com.beans.User;
 @Repository("userDao")
 public class UserDaoImp implements UserDao {
 
+	
+	private Session session;
+//	private EntityManager em;
+	
+	public UserDaoImp() {
+		// TODO Auto-generated constructor stub
+		Configuration configuration = new Configuration();
+		session = configuration.configure().buildSessionFactory().openSession();
+//		em = new Persistence().createEntityManagerFactory("user-jsf").createEntityManager();
+	}
+	
 	@Override
 	public void addUser(User user) {
 		// TODO Auto-generated method stub
 		System.out.println("add");
+//		em.getTransaction().begin();
+//		em.persist(user);
+//		em.getTransaction().commit();
+		
+		session.beginTransaction();
+		session.persist(user);
+		session.getTransaction().commit();
 	}
 
 	@Override
